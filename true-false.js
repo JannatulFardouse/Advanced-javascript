bc569dffb58970dea8d12d11380c0cf42418df47
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2020-04-19 08:48:49 +0200

    true-false done

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//Falsy:
+//0
+//null
+//NaN
+//""
+//undefined
+//false
+//Truthy:
+//'0', [], ' ', 'false'
+
+let name = 0;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
