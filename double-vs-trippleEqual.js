751c05a4725cbf1864ca25eb3a0e2acf46b95ae1
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2020-04-19 21:09:02 +0200

    double vs triple eual

---
+++ double-vs-trippleEqual.js
@@ -0,0 +1,22 @@
+// const first = 2;
+// const second = '2';
+//const first =1;
+//const second = true;
+//const first = false;
+//const second = 0;
+// if(first == second){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+//triple equal always check both type and value
+const first = 2;
+const second = '2';
+if(first === second){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
