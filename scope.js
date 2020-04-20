9cfed393f4a98ba12c5a2f9d392fa2770d7e3dd5
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2020-04-20 13:14:49 +0200

    scope is done

---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus = 20
+function sum(first, second){
+     let result = first + second + bonus;
+//     return result;
+
+// }
+// const add = sum(5, 10);
+// console.log(add);
+
+if(result > 9){
+    //let mood = "happy"; let is not applicable here, let & const are scope variable
+    var mood = "happy" // var is flexible and its a westing variable
+    mood = "Crazy";
+    mood = "Funny"
+
+    }
+    console.log(mood);
+    return result;
+}
+
+const add = sum(5, 10);
+console.log(add);
