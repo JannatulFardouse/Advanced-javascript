33ef50ab9a13c2593b785444ec3af94460fc6a7c
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2020-04-20 12:56:34 +0200

    slice and splice done

---
+++ array-split.js
@@ -0,0 +1,13 @@
+const nums = [2, 4, 6, 8, 10, 12, 14, 15, 16, 17]
+// const part = nums.splice(2, 5);
+// console.log(part);
+// console.log(nums);
+
+// const remove = nums.splice(2, 5);
+// console.log(remove);
+// console.log(nums);
+
+
+const replace = nums.splice(2, 3, 7, 9, 11);
+console.log(replace);
+console.log(nums);
